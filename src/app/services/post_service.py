44dcaf20b83fb6834233ea8b3fa9db9ec71e7c1e
iamwoosung <imws.jo@gmail.com>
2025-07-06 18:53:50 +0900

    ♻️ 게시물 서비스 분기
    ♻️ 게시물 서비스 분기

---
+++ src/app/services/post_service.py
@@ -0,0 +1,102 @@
+from sqlalchemy import select
+
+from fastapi import Depends
+from app.schemas.post import PostCreate, PostUpdate
+from sqlalchemy.orm import Session
+from app.database import get_db
+
+from app.models.post import Post
+from app.schemas.post import PostCreate
+
+class PostService:
+    def __init__(self, db: Session):
+        self.db = db
+
+    """
+    게시글 생성
+    """
+    def create_post(self, post: PostCreate):
+        created_post = Post(**post.model_dump())
+        
+        self.db.add(created_post)
+        self.db.commit()
+        self.db.refresh(created_post)
+
+        return created_post
+    
+
+    """
+    전체 게시글 조회
+    """
+    def get_posts(self):
+        query = (
+            select(Post). 
+            order_by(Post.create_at.desc())
+        )
+        posts = self.db.execute(query).scalars().all()
+
+        return posts
+    
+    """
+    특정 게시글 조회
+    """
+    def get_post(self, post_id: int):
+        
+        query = (
+            select(Post).
+            where(Post.id == post_id)
+        )
+        post = self.db.execute(query).scalar_one_or_none()
+
+        return post
+    
+    """
+    게시글 수정
+    작성자만 수정 가능
+    """
+    def update_post(self, post_id: int, post_update: PostUpdate):
+        query = (
+            select(Post).
+            where(Post.id == post_id)
+        )
+        post = self.db.execute(query).scalar_one_or_none()
+
+        if post is None:
+            return None
+        
+        
+        update_dict = {
+            key: value
+            for key, value in post_update.model_dump().items()
+            if value is not None
+        }
+
+        for key, value in update_dict.items():
+            setattr(post, key, value)
+
+        self.db.commit()
+        self.db.refresh(post)
+
+        return post
+    
+    """
+    게시글 삭제
+    작성자만 삭제 가능
+    """
+    def delete_post(self, post_id: int):
+        query = (
+            select(Post).
+            where(Post.id == post_id)
+        )
+        post = self.db.execute(query).scalar_one_or_none()
+
+        if post is None:
+            return False
+        
+        self.db.delete(post)
+        self.db.commit()
+
+        return True
+
+def get_post_service(db: Session = Depends(get_db)):
+    return PostService(db)
